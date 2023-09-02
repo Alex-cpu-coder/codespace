b7334b19f04a99520e81f5fbd55ba2ef65d5876e
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 05:02:57 +0000

    Sat, Sep 2, 2023, 5:02 AM +00:00

--- swap.c
+++ swap.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main (void)
 {
-    printf("Hello World\n");
+    int a = 10, b = 20;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("after swapping value is a = %d b = %d\n",a,b);
+    
+    
+
+
 }
