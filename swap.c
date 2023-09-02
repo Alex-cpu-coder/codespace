b658eec9c15ffab2f32bb0d8e65136c3e6ff5f9e
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 05:05:55 +0000

    Sat, Sep 2, 2023, 5:05 AM +00:00

--- swap.c
+++ swap.c
@@ -2,12 +2,12 @@
 int main (void)
 {
     int a = 10, b = 20;
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    a = a + b; // 30 = 10 + 10
+    b = a - b; // 20 = 30 - 20
+    a = a - b; // 10 = 20 - 10
     printf("after swapping value is a = %d b = %d\n",a,b);
-    
-    
+ 
+
 
 
 }
