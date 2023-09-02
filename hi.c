f97ff92ef5c7db611778b9b643fb20ab588d269f
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:43:30 +0000

    Sat, Sep 2, 2023, 6:43 AM +00:00

--- hi.c
+++ hi.c
@@ -5,14 +5,14 @@ int main (void){
 int n , a , b , c, rev;
 printf("enter the value of n \n");
 scanf("%d", &n);
-// a = n %10;
-// n = n/10;
+a = n %10;
+n = n/10;
 
-// b = n %10;
-// n = n/10;
+b = n %10;
+n = n/10;
 
-// c = n %10;
-// n = n / 10;
+c = n %10;
+n = n / 10;
 
 // sum = a + b + c;
 // printf("sum = %d\n",sum);
