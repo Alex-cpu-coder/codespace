2d2f95d66000bf9b635865de627c9a2982139d39
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:24:29 +0000

    Sat, Sep 2, 2023, 6:24 AM +00:00

--- hello.c
+++ hello.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 int main(void)
 {
-    int a,b,;
-    printf("enter the value of a and b \n");
-    scanf("%d %d", &a , & b);
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    printft("after swapping value is a = %d b  = %d \n", a, b);
+    // int a,b,;
+    // printf("enter the value of a and b \n");
+    // scanf("%d %d", &a , & b);
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+    // printft("after swapping value is a = %d b  = %d \n", a, b);
 
+    // int a , b , c;
+    // printf("enter the value of a and b \n");
+    // scanf("%d %d", &a , &b);
+    // c = a;
+    // a = b;
+    // b = c;
+    // printf("after swapping value is a = %d b = %d \n", a,b );
 
 
 
