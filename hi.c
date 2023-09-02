e10d16b545a3af64d1e6df8932c81a04316f28dc
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:34:49 +0000

    Sat, Sep 2, 2023, 6:34 AM +00:00

--- hi.c
+++ hi.c
@@ -1,20 +1,29 @@
-int n , a , b, c , sum;
+#include <stdio.h>
 
+int main (void){
+
+
+// int n , a , b, c , sum;
+  int n , a , b , c, rev;
 printf("enter the value of n \n");
 scanf("%d", &n);
-a = n %10;
-n = n/10;
+// a = n %10;
+// n = n/10;
 
-b = n %10;
-n = n/10;
+// b = n %10;
+// n = n/10;
 
-c = n %10;
-n = n / 10;
+// c = n %10;
+// n = n / 10;
 
-sum = a + b + c;
-printf("sum = %d",sum);
+// sum = a + b + c;
+// printf("sum = %d\n",sum);
+
+rev = (a*100) + (b*10) + (c * 1);
+printf("the reverse value of n = %d",rev);
 
 
+}
 
 
 
