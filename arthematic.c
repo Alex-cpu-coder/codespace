00b081e4376ce89f987695550e8dad9b10ade14c
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 05:16:03 +0000

    Sat, Sep 2, 2023, 5:16 AM +00:00

--- arthematic.c
+++ arthematic.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main (void)
 {
-    char a[15] = "Hello Latur\n";
-    printf("%s",a);
+    // char a[15] = "Hello Latur\n";
+    // printf("%s",a);
+
+    float a = 3.14;
+    printf("the value of pi = %f\n",a);
+
 
 }
