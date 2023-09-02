614e0ac26df9430478247dec634dd5bb47c47c78
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:04:27 +0000

    Sat, Sep 2, 2023, 6:04 AM +00:00

---
+++ input.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main (void)
+{
+    int a , b, c;
+    printf("enter the value of a and b : \n");
+    scanf("%d %d ", &a, &b);
+    c = a + b;
+    printf("addition  = %d\n",c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
