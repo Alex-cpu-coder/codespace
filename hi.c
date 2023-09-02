de09edac0a02a3f2392453cd77e3945688684666
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:44:14 +0000

    Sat, Sep 2, 2023, 6:44 AM +00:00

--- hi.c
+++ hi.c
@@ -17,7 +17,7 @@ n = n / 10;
 // sum = a + b + c;
 // printf("sum = %d\n",sum);
 
-rev = (a*10) + (b*10) + (c * 1);
+rev = (a*100) + (b*10) + (c * 1);
 printf("the sum of  reverse value of n is = %d",rev);
 
 
