44b3de6f59de9eaa73c4e8a004fb32de2e0116ef
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:19:30 +0000

    Sat, Sep 2, 2023, 6:19 AM +00:00

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,;
+    printf("enter the value of a and b \n");
+    scanf("%d %d", &a , & b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printft("after swapping value is a = %d b  = %d \n" a, b);
+    
+
+}
