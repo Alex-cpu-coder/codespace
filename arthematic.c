6e1d5daa35c0c3aaa5cef823715c33521fe630f6
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 05:25:55 +0000

    Sat, Sep 2, 2023, 5:25 AM +00:00

--- arthematic.c
+++ arthematic.c
@@ -6,9 +6,14 @@ int main (void)
 
     // float a = 3.14;
     // printf("the value of pi = %f\n",a);
-    float x = 10.5;
-    float y = 7.5;
-    printf("the value of x + y = %d\n",x + y);
+    //float x = 10.5;
+    //float y = 7.5;
+    // printf("the value of x + y = %f\n",x + y);
 
+    int a = 10 , b = 20 , c;
+    c = a;
+    a = b;
+    b = c;
+    printf("value of swapping = a = %d b = %d \n", a ,b );
 
 }
