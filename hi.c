7074d0482da948da3f84a7e7eb6aa8cbe30db041
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-02 06:30:04 +0000

    Sat, Sep 2, 2023, 6:30 AM +00:00

---
+++ hi.c
@@ -0,0 +1,29 @@
+int n , a , b, c , sum;
+
+printf("enter the value of n \n");
+scanf("%d", &n);
+a = n %10;
+n = n/10;
+
+b = n %10;
+n = n/10;
+
+c = n %10;
+n = n / 10;
+
+sum = a + b + c;
+printf("sum = %d",sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
