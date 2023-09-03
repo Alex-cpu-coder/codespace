6690e6565eeb795680432712a8014aeacfa966a4
Alex-cpu-coder <142068401+Alex-cpu-coder@users.noreply.github.com>
2023-09-03 13:57:19 +0000

    Sun, Sep 3, 2023, 1:57 PM +00:00

--- my.c
+++ my.c
@@ -2,6 +2,9 @@
 int main(void)
 {
     int x , y;
-    printft("enter the value of x and y );
-    
+    printf("enter the value of x and y\n" );
+    scanf("%d", &x , &y);
+    int z = x + y;
+    printf("the sum of x and y %d\n",z);
+    return 0;
 }
